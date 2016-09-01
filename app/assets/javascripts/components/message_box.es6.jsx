a3fffa4f1b6bd2aa655278ddbfb765f0103e818b
Noriyo Akita <noriyo.akita@gmail.com>
2016-09-01 08:50:12 +0000

    Improve handleMessageSubmit to post a new message to server side

--- app/assets/javascripts/components/message_box.es6.jsx
+++ app/assets/javascripts/components/message_box.es6.jsx
@@ -6,9 +6,19 @@ class MessageBox extends React.Component {
   }
 
   handleMessageSubmit(message) {
-    message.id = new Date();
-    let newMessages = this.state.messages.concat(message);
-    this.setState({ messages: newMessages });
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: message,
+      success: function(message) {
+        let newMessages = this.state.messages.concat(message);
+        this.setState({ messages: newMessages });
+      }.bind(this),
+      error: function(_xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   }
 
   render () {
