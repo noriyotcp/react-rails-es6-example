f88797dd083d2c3fc825e497ad3478933c894e98
Noriyo Akita <noriyo.akita@gmail.com>
2016-09-01 08:33:46 +0000

    Add index and create to MessagesController

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,21 @@
 class MessagesController < ApplicationController
+  def index
+    messages = Message.all
+    render json: messages
+  end
+
+  def create
+    message = Message.new(create_params)
+    if message
+      render json: message, status: :created
+    else
+      render json: message, status: :unproccessable_entity
+    end
+  end
+
+  private
+
+  def create_params
+    params.permit(:user, :text)
+  end
 end
