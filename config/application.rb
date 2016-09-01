6b5060c6811aa82a80a30d4f17190f3f3ab612b0
Noriyo Akita <noriyo.akita@gmail.com>
2016-09-01 07:29:28 +0000

    Setting for React, Babel

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module ReactRailsEs6Example
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # browserify_rails option
+    config.browserify_rails.commandline_options = '-t babelify'
   end
 end
