754f4a57036494c2ca0cda5d5bf991316a25f797
Noriyo Akita <noriyo.akita@gmail.com>
2016-09-01 08:26:01 +0000

    [message_form] Add handleSubmit
    to get values from form

--- app/assets/javascripts/components/message_form.es6.jsx
+++ app/assets/javascripts/components/message_form.es6.jsx
@@ -1,9 +1,29 @@
 class MessageForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let user = ReactDOM.findDOMNode(this.refs.user).value.trim();
+    let text = ReactDOM.findDOMNode(this.refs.text).value.trim();
+    // Do nothing without user or text
+    if (!user || !text) {
+      return;
+    }
+    // Call the event on MessageBox
+    this.props.onMessageSubmit({ user: user, text: text });
+    // Remove the values on form
+    this.refs.user.value = '';
+    this.refs.text.value = '';
+  }
+
   render () {
     return (
-      <form className="commentForm">
-        <input type="text" placeholder="Your name" />
-        <input type="text" placeholder="Message" />
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="user" />
+        <input type="text" placeholder="Message" ref="text" />
         <input type="submit" value="Post" />
       </form>
     );
