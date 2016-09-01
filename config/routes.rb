02db841478d87e77a61ca2dd72d535afdf6dc2fc
Noriyo Akita <noriyo.akita@gmail.com>
2016-09-01 08:28:09 +0000

    Generate Message as resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :messages, only: [:index, :create], format: :json
   root 'top#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
