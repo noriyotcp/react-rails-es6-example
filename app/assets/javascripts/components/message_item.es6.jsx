527a598b93e751fcae80272769c91e240bc65d52
Noriyo Akita <noriyo.akita@gmail.com>
2016-09-01 08:21:37 +0000

    [message_item] Remove useless whitespace

--- app/assets/javascripts/components/message_item.es6.jsx
+++ app/assets/javascripts/components/message_item.es6.jsx
@@ -2,7 +2,7 @@ class MessageItem extends React.Component {
   render () {
     return (
       <div className="message">
-        <h2 className="messageUser">{this.props.message.user} </h2>
+        <h2 className="messageUser">{this.props.message.user}</h2>
         <span>{this.props.message.text}</span>
       </div>
     );
